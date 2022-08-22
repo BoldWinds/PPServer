eaa6fbbb92ca42cc5a04a73bcf3a1e221321677b
LBWyes <88589340+LBWyes@users.noreply.github.com>
2022-08-22 16:16:58 +0800

    openServer when project starts

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "serversingleton.h"
+#include "sqlmanipulation.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+
+    ServerSingleton::getInstance()->openServer();
+    sqlManipulation::instantiation();
 
     return a.exec();
 }
